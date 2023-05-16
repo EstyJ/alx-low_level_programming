fe3be2b18d5296a955c91afc49de41c7241830cd
EstyJ <joshuaesther251@gmail.com>
2023-05-15 19:49:19 -0700

    0x09 static libries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string to stdout, followed by a new line.
+ * @str: pointer to the string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		putchar(*str);
+	}
+
+	putchar('\n');
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+This directory contain codes for the 0x09-static_libraries project
